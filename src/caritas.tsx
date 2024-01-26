368cb91c968e8372a4c2becfa63c703dacf382e0
Josue MTL <116099684+JosueMTL@users.noreply.github.com>
2024-01-26 09:06:43 -0500

    first commit

---
+++ src/caritas.tsx
@@ -0,0 +1,17 @@
+// import React, { useState } from 'react';
+
+// export default function EmojiIncrement() {
+//     const [emojiString, setEmojiString] = useState('🤣');
+
+//     return (
+//         <>
+//             <h1>{emojiString}</h1>
+//             <button onClick={() => {
+//                 setEmojiString(prevEmoji => prevEmoji + "🤣");
+//                 setEmojiString(prevEmoji => prevEmoji + "🤣");
+//                 setEmojiString(prevEmoji => prevEmoji + "🤣");
+//             }}>Add 3 Caritas</button>
+//         </>
+//     );
+// }
+
---
+++ src/useState.tsx
@@ -0,0 +1,12 @@
+// import React, { useState } from 'react';
+
+// function SpookyCounter() {
+//     const [SpookyCount, setSpookyCount] = useState(0);
+
+//     return (
+//         <div>
+//             <p>Conteo espelusnante: {SpookyCount}</p>
+//             <button onClick={() => setSpookyCount(SpookyCount + 1)}>Aumentar Conteo</button>
+//         </div>
+//     );
+// }
